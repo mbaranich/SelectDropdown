216086d48ff692321023a456999b572a60255d95
Matt B <mbaranich@yahoo.com>
2021-07-11 02:48:03 -0400

    Add custom select functionality.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import Select from './select.js'
 
-const selectElements = document.querySelectorAll('[data-custom]')
+const selectElements = document.querySelectorAll("[data-custom]")
 
 selectElements.forEach(selectElement => {
-    console.log(new Select(selectElement))
+    new Select(selectElement)
 })
 
 
