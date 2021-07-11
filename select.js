319cdb50c48439e956ba230614e0f08b6a1050b0
Matt B <mbaranich@yahoo.com>
2021-07-10 22:42:57 -0400

    Setup custom Select element.

--- select.js
+++ select.js
@@ -1,20 +1,47 @@
 export default class Select {
     constructor(element) {
         this.element = element
+        this.options = getFormattedoptions(element.querySelectorAll('option'))
         this.customElement = document.createElement('div')
         this.labelElement = document.createElement('span')
         this.optionsCustomElement = document.createElement('ul')
         setupCustomElement(this)
+        element.after(this.customElement)
+
+    }
+
+    get selectedOption() {
+        return this.options.find(option => option.selected)
     }
 }
 
 function setupCustomElement(select) {
-    select.customElement.classlist.add('custom-select-container')
+    select.customElement.classList.add('custom-select-container')
 
-    select.labelElement.classlist.add('custom-select-value')
+    select.labelElement.classList.add('custom-select-value')
+    select.labelElement.innerText = select.selectedOption.label
     select.customElement.append(select.labelElement)
 
-    select.optionsCustomElement.classlist.add('custom-select-options')
+    select.optionsCustomElement.classList.add('custom-select-options')
+    select.options.forEach(option => {
+        const optionElement = document.createElement('li')
+        optionElement.classList.add('custom-select-option')
+        optionElement.classList.toggle('selected', option.selected)
+        optionElement.innerText = option.label
+        optionElement.dataset.value = option.value
+        select.optionsCustomElement.append(optionElement)
+    })
     select.customElement.append(select.optionsCustomElement)
-    
+
+}
+
+function getFormattedoptions(optionElements) {
+    return [...optionElements].map(optionElement => {
+        return {
+            value: optionElement.value,
+            label: optionElement.label,
+            selected: optionElement.selected,
+            element: optionElement
+        }
+    })
 }
